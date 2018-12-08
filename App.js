cc45bcb349a76c58e341a377b26814dee27a58f9
doums <dommerc.pierre@gmail.com>
2018-12-08 17:19:22 +0100

    added splash screen and kotlin

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import MainContextManager from './components/mainContextManager'
 import { YellowBox } from 'react-native'
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text
+} from 'react-native'
 
 YellowBox.ignoreWarnings(['Require cycle:'])
 
 const App = () => {
-  return <MainContextManager />
+  return <View style={{ backgroundColor: 'black', flex: 1 }}/>
 }
 export default App
--- android/app/src/main/java/com/final_app/MainActivity.java
+++ android/app/src/main/java/com/final_app/MainActivity.java
@@ -1,8 +1,19 @@
 package com.final_app;
 
+
+
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Make sure this is before calling super.onCreate
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        // ...
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
